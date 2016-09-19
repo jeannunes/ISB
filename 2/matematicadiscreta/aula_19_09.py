f1c0341335cdcb211bb055ed82da10f7c1a1f104
Jean Nunes <jeanrnunes@gmail.com>
2016-09-19 20:38:02 -0300

    Update aula_19_09.py

--- 2/matematicadiscreta/aula_19_09.py
+++ 2/matematicadiscreta/aula_19_09.py
@@ -1,1 +1,27 @@
-#
+from sympy import *
+from IPython.display import display
+#import numpy as n
+#import matplotlib.pyplot as p
+
+x = Symbol('x')
+
+# Criação de listas automáticas
+lista = [ (x, f(x)) for x in range(20) ]
+
+# Retorna a função geral das derivadas de acordo com a ordem
+def derivadaGeral(n): # N é a ordem da derivada f**n(x)
+    resto = n%4
+    if resto == 0:
+        return sin(x)
+    elif resto == 1:
+        return cos(x)
+    elif resto == 2:
+        return -sin(x)
+    else:
+        return -cos(x)
+        
+def f(n):
+    return n
+
+def v(d, t):
+    return d/t
