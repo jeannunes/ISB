e983d3b30fae3d1b9c4059b00483140945ba7db4
Jean Nunes <jeanrnunes@gmail.com>
2016-09-20 19:40:31 -0300

    Update 20.09.py

--- 2/matematicadiscreta/20.09.py
+++ 2/matematicadiscreta/20.09.py
@@ -32,3 +32,18 @@ ordem = [ der(sin(y)) for y in range(0,10,1) ]
     c) Escreva um programa em Python que retorne a derivada de ordem n da
         função sin(x)
 """
+
+# f(x) = (x**3/3) + (x**2/2) - 2*x
+
+def f2(n):
+    return (n**3/3) + (n**2/2) - 2*n
+def g2(n):
+    return n**2 + n - 2
+def der2(n):
+    return diff(n)
+
+order = [ (y, g(y)) for y in range(-10,10) ]
+         
+    # Ponto Crítico em X - 1
+    # Crescente em X != 0 & x != -1
+    # Decrescente em 0 e 1
