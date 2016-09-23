d01c213f35faecde5960f71601a430cabc77b8ab
Jean Nunes <jeanrnunes@gmail.com>
2016-09-23 19:45:20 -0300

    Create Peca.java

---
+++ 2/programming/jogodavelha/Peca.java
@@ -0,0 +1,19 @@
+public class Peca {
+	
+	private char simbolo;
+
+	public Peca(char peca){
+		this.setSimbolo(peca);
+	}
+	
+	public char getSimbolo() {
+		return simbolo;
+	}
+
+	public void setSimbolo(char simbolo) {
+		this.simbolo = simbolo;
+	}
+	
+	
+
+}
