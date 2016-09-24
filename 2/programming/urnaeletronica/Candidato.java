d76f512b8380fdca923a900c597f8ceb5eec8418
Jean Nunes <jeanrnunes@gmail.com>
2016-09-23 21:12:07 -0300

    Create Candidato.java

---
+++ 2/programming/urnaeletronica/Candidato.java
@@ -0,0 +1,28 @@
+public class Candidato {
+	
+	private String nome, partido;
+
+	public Candidato(String nome, String pardito){
+		this.setNome(nome);
+		this.setPartido(pardito);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPartido() {
+		return partido;
+	}
+
+	public void setPartido(String partido) {
+		this.partido = partido;
+	}
+	
+	
+
+}
