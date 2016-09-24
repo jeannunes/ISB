0ea8560fa1c47c47d1f4bef3e238328a98bb95b8
Jean Nunes <jeanrnunes@gmail.com>
2016-09-24 19:09:01 -0300

    Update Candidato.java

--- 2/programming/urnaeletronica/Candidato.java
+++ 2/programming/urnaeletronica/Candidato.java
@@ -1,28 +1,34 @@
-public class Candidato {
+public class Administrador {
 	
-	private String nome, partido;
-
-	public Candidato(String nome, String pardito){
+	private int codigo, senha;
+	private String nome;
+	
+	public Administrador(int codigo, String nome, int senha){
+		this.setCodigo(codigo);
 		this.setNome(nome);
-		this.setPartido(pardito);
+		this.setSenha(senha);
 	}
 	
+	public int getCodigo() {
+		return codigo;
+	}
+	public boolean setCodigo(int codigo) {
+		if ( codigo > 0 ) {
+			this.codigo = codigo;
+			return true;
+		}
+		return false;
+	}
+	public int getSenha() {
+		return senha;
+	}
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public String getPartido() {
-		return partido;
-	}
-
-	public void setPartido(String partido) {
-		this.partido = partido;
-	}
-	
-	
-
 }
