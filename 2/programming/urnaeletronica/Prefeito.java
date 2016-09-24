f8b436787ca47e1b08e4342d51fb6cc9728063f6
Jean Nunes <jeanrnunes@gmail.com>
2016-09-24 19:09:51 -0300

    Update Prefeito.java

--- 2/programming/urnaeletronica/Prefeito.java
+++ 2/programming/urnaeletronica/Prefeito.java
@@ -1,10 +1,11 @@
 public class Prefeito extends Candidato {
 
-	private int numero;
+	private int numero, votos;
 
 	public Prefeito(String nome, String partido, int numero){
 		super(nome, partido);
 		this.setNumero(numero);
+		this.votos = 0;
 	}
 	
 	public int getNumero() {
@@ -12,8 +13,18 @@ public class Prefeito extends Candidato {
 	}
 
 	public boolean setNumero(int numero) {
-		if (numero > 0 && numero < 100) {
+		if (numero >= -1 && numero < 100) {
 			this.numero = numero;
+			return true;
+		}
+		return false;
+	}
+	public int getVotos(){
+		return this.votos;
+	}
+	public boolean votar(int numero){
+		if (this.numero == numero) {
+			this.votos =+1;
 			return true;
 		}
 		return false;
