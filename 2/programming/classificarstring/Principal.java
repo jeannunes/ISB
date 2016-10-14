f5c2868770ee6addca5ab9de38b51ffe2fc1a1ea
Jean Nunes <jeanrnunes@gmail.com>
2016-10-14 20:43:50 -0300

    Create Principal.java

---
+++ 2/programming/classificarstring/Principal.java
@@ -0,0 +1,30 @@
+package exercicio2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Classificacao strings;
+		String[] conteudo;
+		String termo;
+		int posicao;
+		
+		conteudo = new String[3];
+		conteudo[0] = "Jean";
+		conteudo[1] = "Lucas";
+		conteudo[2] = "Andrey";
+		
+		strings = new Classificacao(conteudo);
+		
+		termo = "Lucas";
+		posicao = strings.buscar(termo);
+		System.out.println(termo + " foi encontrado na posição " + posicao);
+		
+		conteudo = strings.cassificar();
+		for(posicao = 0; posicao < conteudo.length; posicao++){
+			System.out.println(posicao + " = " + conteudo[posicao]);
+		}
+
+	}
+
+}
