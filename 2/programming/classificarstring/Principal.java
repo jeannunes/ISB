40b7e9d6706e7b3324cd84deb5c3644026f2f1a8
Jean Nunes <jeanrnunes@gmail.com>
2016-10-14 20:52:58 -0300

    Update Principal.java

--- 2/programming/classificarstring/Principal.java
+++ 2/programming/classificarstring/Principal.java
@@ -20,7 +20,7 @@ public class Principal {
 		posicao = strings.buscar(termo);
 		System.out.println(termo + " foi encontrado na posição " + posicao);
 		
-		conteudo = strings.cassificar();
+		conteudo = strings.classificar();
 		for(posicao = 0; posicao < conteudo.length; posicao++){
 			System.out.println(posicao + " = " + conteudo[posicao]);
 		}
